8cc692b9ee263f7b16ed60f19eb1df3246f488ec
chan <heechan.log@gmail.com>
2025-05-21 17:26:24 +0900

    프로젝트 이름변경 todo -> Market

--- src/main/java/com/market/Main.java
+++ src/main/java/com/market/Main.java
@@ -1,13 +1,13 @@
-package com.todo.demo;
+package com.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
