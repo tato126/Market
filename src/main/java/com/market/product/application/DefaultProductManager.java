78e1e7ece36291cb4c534285d16a0400ed39a9f4
chan <heechan.log@gmail.com>
2025-05-23 15:24:42 +0900

    수정된 상품(Product) 유스케이스 인터페이스 와 구현체 클래스

--- src/main/java/com/market/product/application/DefaultProductManager.java
+++ src/main/java/com/market/product/application/DefaultProductManager.java
@@ -1,8 +1,10 @@
 package com.market.product.application;
 
-import com.market.product.domain.Product;
-import com.market.product.domain.ProductId;
+import com.market.product.domain.*;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -10,30 +12,42 @@ import java.util.List;
  *
  * @author chan
  */
+@Transactional
+@Service
 public class DefaultProductManager implements ProductRegistry, ProductFind, ProductModification, ProductCleanUp {
 
-    @Override
-    public void clear(ProductId productId) throws RuntimeException {
+    private final ProductIdGenerator productIdGenerator;
+    private final ProductRepository productRepository;
 
+    public DefaultProductManager(ProductIdGenerator productIdGenerator, ProductRepository productRepository) {
+        this.productIdGenerator = productIdGenerator;
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductId register(String sellerName, String productName, String description, BigDecimal price, Integer stockQuantity, ProductState state, ProductCategory productCategory) throws RuntimeException {
+        return productRepository.save(Product.create(productIdGenerator, sellerName, productName, description, price, stockQuantity, state, productCategory)).getId();
     }
 
     @Override
     public List<Product> all() {
-        return List.of();
+        return productRepository.findAll();
     }
 
+    // TODO: 예외 처리를 EntityNotFoundException 형식으로 변경해야 한다.
     @Override
     public Product byId(ProductId productId) throws RuntimeException {
-        return null;
+        return productRepository.findById(productId).orElseThrow(() -> new RuntimeException());
     }
 
+    @Override
+    public void clear(ProductId productId) throws RuntimeException {
+        productRepository.delete(byId(productId));
+    }
+
+    // 현재는 다루지 않음
     @Override
     public void modify(ProductId productId) throws RuntimeException {
-
-    }
-
-    @Override
-    public Product register(String productName) throws RuntimeException {
-        return null;
+//        productRepository.findById(byId(productId).update());
     }
 }
--- src/main/java/com/market/product/application/ProductRegistry.java
+++ src/main/java/com/market/product/application/ProductRegistry.java
@@ -1,6 +1,10 @@
 package com.market.product.application;
 
-import com.market.product.domain.Product;
+import com.market.product.domain.ProductCategory;
+import com.market.product.domain.ProductId;
+import com.market.product.domain.ProductState;
+
+import java.math.BigDecimal;
 
 /**
  * 상품 등록 유스케이스.
@@ -11,6 +15,7 @@ public interface ProductRegistry {
 
     // TODO: 상품 객체를 반환해야 하는가? 아니면 일련번호를 반환해야하는가?
     // 예시: ProductId or Product
+
     /**
      * 새로운 상품 등록.
      *
@@ -18,5 +23,5 @@ public interface ProductRegistry {
      * @return 상품 객체.
      */
     // TODO: 예외 처리를 RegistrationException 형식으로 변경해야 한다.
-    Product register(String productName) throws RuntimeException;
+    ProductId register(String sellerName, String productName, String description, BigDecimal price, Integer stockQuantity, ProductState state, ProductCategory productCategory) throws RuntimeException;
 }
