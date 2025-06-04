b09ba8b52983471e2feecef9d75fc32c6c6a1a78
chan <heechan.log@gmail.com>
2025-06-04 17:45:41 +0900

    TestRestTemplate를 통한 실제 서버 환경에서의 페이지 로딩 테스트이다.

--- src/main/java/com/market/web/IndexController.java
+++ src/main/java/com/market/web/IndexController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MarketController {
+public class IndexController {
 
     @GetMapping("/")
-    public String market() {
+    public String index() {
         return "index";
     }
-
 }
