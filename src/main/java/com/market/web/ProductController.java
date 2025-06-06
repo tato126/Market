28366ad180fcf97db9d3cd0d1f38ca836c8aa559
chan <heechan.log@gmail.com>
2025-06-06 17:24:50 +0900

    등록된 상품을 Find(전체 조회)하여 가져온다.

--- src/main/java/com/market/web/ProductController.java
+++ src/main/java/com/market/web/ProductController.java
@@ -1,17 +1,38 @@
 package com.market.web;
 
+import com.market.core.product.application.ProductFind;
+import com.market.core.product.domain.Product;
+import com.market.web.dto.response.ProductResponseDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
 
+    private final ProductFind productFind;
+
+    public ProductController(ProductFind productFind) {
+        this.productFind = productFind;
+    }
+
     @GetMapping("/products")
-    public String product() {
+    public String product(Model model) {
+
+        List<Product> products = productFind.all();
+
+        List<ProductResponseDto> productsForView = products.stream()
+                .map(ProductResponseDto::new)
+                .toList();
+
+        model.addAttribute("products", productsForView);
+
         return "products";
     }
 
-    @GetMapping("/product/save")
+    @GetMapping("/products/save")
     public String productsRegistry() {
         return "products-registry";
     }
