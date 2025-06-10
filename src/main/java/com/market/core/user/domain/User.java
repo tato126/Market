1d8806c0d84dc3901c5e82afd80eee96a1693223
chan <heechan.log@gmail.com>
2025-06-10 18:03:40 +0900

    OAuth2 인증에서는 email을 사용함으로 비밀번호(password) 필드를 대신하여 email을 생성자에 추가하고 관리한다.

--- src/main/java/com/market/core/user/domain/User.java
+++ src/main/java/com/market/core/user/domain/User.java
@@ -14,7 +14,7 @@ public class User {
 
     private String username;
 
-    private String password;
+    private String email;
 
     private UserPicture profilePicture;
 
@@ -23,49 +23,43 @@ public class User {
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 
-    public User(UserId userId, String username, String password, UserPicture profilePicture, Role userRole, LocalDateTime createdDate) {
+    public User(UserId userId, String username, String email, UserPicture profilePicture, Role userRole, LocalDateTime createdDate) {
         this.userId = Objects.requireNonNull(userId, "UserId must not be null");
         this.username = Objects.requireNonNull(username, "Username must not be null");
-        this.password = Objects.requireNonNull(password, "UserPassword must not be null");
-        this.profilePicture = Objects.requireNonNull(profilePicture, "ProfilePicture must not be null");
+        this.email = Objects.requireNonNull(email, "UserPassword must not be null");
+        this.profilePicture = profilePicture; // 프로필 이미지는 null 일 수 있다.
         this.userRole = Objects.requireNonNull(userRole, "UserRole must not be null");
         this.createdDate = Objects.requireNonNull(createdDate, "User createDate must not be null");
         this.lastModifiedDate = this.createdDate;
     }
 
     // for Hibernate
-    public User() {
+    protected User() {
     }
 
-    public static User create(UserIdGenerator idGenerator, String username, String password, UserPicture profilePicture, Role userRole) {
+    public static User create(UserIdGenerator idGenerator, String username, String email, UserPicture profilePicture, Role userRole) {
 
         return new User(
                 idGenerator.generatorId(),
                 username,
-                password,
+                email,
                 profilePicture,
                 userRole,
                 LocalDateTime.now()
         );
     }
 
-    public void update(String username, String password, UserPicture profilePicture, Role userRole) {
+    public User update(String username, UserPicture profilePicture) {
 
         if (username != null) {
             this.username = username;
-        }
-
-        if (password != null) {
-            this.password = password;
         }
 
         if (profilePicture != null) {
             this.profilePicture = profilePicture;
         }
 
-        if (userRole != null) {
-            this.userRole = userRole;
-        }
+        return this;
     }
 
     // Getter
@@ -77,8 +71,8 @@ public class User {
         return username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getEmail() {
+        return email;
     }
 
     public UserPicture getProfilePicture() {
@@ -95,5 +89,9 @@ public class User {
 
     public LocalDateTime getLastModifiedDate() {
         return lastModifiedDate;
+    }
+
+    public String getRoleKey() {
+        return this.userRole.getKey();
     }
 }
